2ff800dde12620fda39a506aabeb34c3644280e0
Olga Uzun <67106495+ol4enok20101@users.noreply.github.com>
2021-04-06 11:21:42 +0300

    Updates

--- static/js/main.b94bb73e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],[function(o,t){}],[[0,1]]]);
-//# sourceMappingURL=main.b94bb73e.chunk.js.map
